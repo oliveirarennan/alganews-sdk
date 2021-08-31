7a701ba51ac761e69de79d1cebf6579ef180445a
Rennan Oliveira <rennan_neves@live.com>
2021-08-31 15:03:29 -0300

    Feat: Update MetricsService

--- src/services/Metric.service.ts
+++ src/services/Metric.service.ts
@@ -2,13 +2,37 @@ import { Metric } from "../@types";
 import Service from "./Services";
 
 export default class MetricService extends Service {
-  static getTop3Tags(){
-    return this.Http.get<Metric.EditorTagRatio>(`/metrics/editor/top3-tags`).then(this.getData)
+  static getTop3Tags() {
+    return this.Http.get<Metric.EditorTagRatio>(
+      `/metrics/editor/top3-tags`
+    ).then(this.getData);
   }
 
-  static getEditorMonthlyEarnings(){
-    return this.Http
-      .get<Metric.EditorMonthlyEarnings>('/metrics/editor/monthly-earnings')
-      .then(this.getData)
+  static getEditorMonthlyEarnings() {
+    return this.Http.get<Metric.EditorMonthlyEarnings>(
+      "/metrics/editor/monthly-earnings"
+    ).then(this.getData);
+  }
+
+  static getMonthlyRevenuesExpenses() {
+    return this.Http.get<Metric.MonthlyRevenuesExpenses>(
+      "/metrics/monthly-revenues-expenses",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(this.getData)
+  }
+
+  static getMonthlyRevenuesExpensesChartJs() {
+    return this.Http.get<Metric.MonthlyRevenuesExpenses>(
+      "/metrics/monthly-revenues-expenses",
+      {
+        headers: {
+          "Content-Type": "application/vnd.alganews.chartjs+json",
+        },
+      }
+    ).then(this.getData)
   }
 }
